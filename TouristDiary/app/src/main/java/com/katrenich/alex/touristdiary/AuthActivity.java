381f66f5cc62d85c41bb4daac07d4033ee56d61a
AlexKatrenich <alex.katrenich@gmail.com>
2018-04-29 21:33:47 +0300

    modified misc.xml

--- TouristDiary/app/src/main/java/com/katrenich/alex/touristdiary/AuthActivity.java
+++ TouristDiary/app/src/main/java/com/katrenich/alex/touristdiary/AuthActivity.java
@@ -1,14 +1,36 @@
 package com.katrenich.alex.touristdiary;
 
-import android.support.v7.app.AppCompatActivity;
+import android.support.annotation.NonNull;
 import android.os.Bundle;
+import android.util.Log;
 
-public class AuthActivity extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class AuthActivity extends LogActivity {
+
+    private FirebaseAuth mAuth; // змінна для роботи з авторизацією
+    private FirebaseAuth.AuthStateListener mAuthListener; // змінна для прослуховування зміни статусу користувача
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_auth);
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            FirebaseUser user;
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                user = firebaseAuth.getCurrentUser();
+                if(user != null){
+                    Log.d(TAG, "onAuthStateChanged: signed_in: " + user.getUid());
+                } else {
+                    Log.d(TAG, "onAuthStateChanged: signed_out ");
+                }
+            }
+        };
     }
+
+
 
 }
