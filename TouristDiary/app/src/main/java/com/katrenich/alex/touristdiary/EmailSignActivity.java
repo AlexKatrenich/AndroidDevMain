7d01d7956a43916c07a09287f35762c4b6d8f6e6
AlexKatrenich <alex.katrenich@gmail.com>
2018-04-29 23:35:18 +0300

    Added new Activity - Sign_in with Email; On AuthActivity added checkbox

---
+++ TouristDiary/app/src/main/java/com/katrenich/alex/touristdiary/EmailSignActivity.java
@@ -0,0 +1,14 @@
+package com.katrenich.alex.touristdiary;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EmailSignActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_email_sign);
+    }
+
+}
