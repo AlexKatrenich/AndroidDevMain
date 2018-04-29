b75c87a6168b0bc0befa436afe2e47db5c58fa4d
AlexKatrenich <alex.katrenich@gmail.com>
2018-04-29 23:15:08 +0300

    Added buttons Sing_in with and initializied it`s in Activity

--- TouristDiary/app/src/main/java/com/katrenich/alex/touristdiary/AuthActivity.java
+++ TouristDiary/app/src/main/java/com/katrenich/alex/touristdiary/AuthActivity.java
@@ -2,15 +2,24 @@ package com.katrenich.alex.touristdiary;
 
 import android.support.annotation.NonNull;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-public class AuthActivity extends LogActivity {
+public class AuthActivity extends LogActivity implements View.OnClickListener{
 
     private FirebaseAuth mAuth; // змінна для роботи з авторизацією
     private FirebaseAuth.AuthStateListener mAuthListener; // змінна для прослуховування зміни статусу користувача
+
+    private Button btnGoogleSignIn;
+    private Button btnFacebookSignIn;
+    private Button btnEmailSignIn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,8 +38,49 @@ public class AuthActivity extends LogActivity {
                 }
             }
         };
+
+        getWindow().getDecorView().setBackground(ContextCompat
+                .getDrawable(this, R.drawable.ic_app_authority_background));
+
+        // ініціалізую кнопки та передаю їм прослуховувач кліку
+        btnGoogleSignIn = findViewById(R.id.btn_google_sign_in);
+        btnGoogleSignIn.setOnClickListener(this);
+        Log.d(TAG, "onCreate: btnGoogleSignIn.setOnClickListener(this)");
+        btnFacebookSignIn = findViewById(R.id.btn_facebook_sign_in);
+        btnFacebookSignIn.setOnClickListener(this);
+        Log.d(TAG, "onCreate: btnFacebookSignIn.setOnClickListener(this)");
+        btnEmailSignIn = findViewById(R.id.btn_email_sign_in);
+        btnEmailSignIn.setOnClickListener(this);
+        Log.d(TAG, "onCreate: btnEmailSignIn.setOnClickListener(this)");
     }
 
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btn_email_sign_in :
+                Toast.makeText(this, "Sign-in with Email button was clicked!", Toast.LENGTH_SHORT).show();
+                Log.d(TAG, "onClick: Sign-in with Email button was clicked!");
+                break;
+            case R.id.btn_facebook_sign_in :
+                Toast.makeText(this, "Sign-in with Facebook  button was clicked!", Toast.LENGTH_SHORT).show();
+                Log.d(TAG, "onClick: Sign-in with Facebook  button was clicked!");
+                break;
+            case R.id.btn_google_sign_in :
+                Toast.makeText(this, "Sign-in with Google button was clicked!", Toast.LENGTH_SHORT).show();
+                Log.d(TAG, "onClick: Sign-in with Google button was clicked!");
+                break;
+            default:
+                break;
+        }
+    }
 
+    @Override
+    protected void onDestroy() {
+        //зануляю посилання на слухача кліків для кнопок
+        btnEmailSignIn.setOnClickListener(null);
+        btnFacebookSignIn.setOnClickListener(null);
+        btnGoogleSignIn.setOnClickListener(null);
+        super.onDestroy();
+    }
 }
