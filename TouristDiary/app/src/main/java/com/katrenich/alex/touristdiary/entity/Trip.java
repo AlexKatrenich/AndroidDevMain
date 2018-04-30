a2cb98db5a1256ca8b7b49602b2fee90685e4f2e
AlexKatrenich <alex.katrenich@gmail.com>
2018-05-01 01:38:37 +0300

    added entity trip

---
+++ TouristDiary/app/src/main/java/com/katrenich/alex/touristdiary/entity/Trip.java
@@ -0,0 +1,11 @@
+package com.katrenich.alex.touristdiary.entity;
+
+import java.util.Date;
+import java.util.List;
+
+public class Trip {
+    private String name;
+    private List<Date> dates;
+    private List<String> notes;
+
+}
