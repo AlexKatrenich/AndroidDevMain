39905131233850d220066c572285157461739f5f
AlexKatrenich <alex.katrenich@gmail.com>
2018-05-21 18:33:33 +0300

    work with fragments, test

--- Fragments/app/src/main/java/com/katrenich/alex/fragments/MyFragmentTwo.java
+++ Fragments/app/src/main/java/com/katrenich/alex/fragments/MyFragmentTwo.java
@@ -1,35 +1,31 @@
 package com.katrenich.alex.fragments;
 
-import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class MyFragment extends Fragment {
+public class MyFragmentTwo extends Fragment {
     private String TAG = "MyLog";
 
     @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        Log.d(TAG, "onAttach");
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
     }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        Log.d(TAG, "onCreateView");
-        return inflater.inflate(R.layout.fragment_my, container, false);
+        return inflater.inflate(R.layout.fragment_my_two, container, false);
     }
+
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        Log.d(TAG, "onViewCreated");
+
         super.onViewCreated(view, savedInstanceState);
     }
-
 }
