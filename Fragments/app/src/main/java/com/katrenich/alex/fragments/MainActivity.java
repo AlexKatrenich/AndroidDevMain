87b63a9176042de87b2268c280ec8acda7017952
AlexKatrenich <alex.katrenich@gmail.com>
2018-05-27 12:55:50 +0300

    works with fragments

--- Fragments/app/src/main/java/com/katrenich/alex/fragments/MainActivity.java
+++ Fragments/app/src/main/java/com/katrenich/alex/fragments/MainActivity.java
@@ -6,10 +6,11 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     BottomNavigationView bottomNavigation;
 
@@ -61,4 +62,20 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btn_open_next_fragment :
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.fl_container_fragment, new MyFragmentTwo())
+                        .addToBackStack(null)
+                        .commit();
+                break;
+
+            default:
+                break;
+        }
+    }
 }
