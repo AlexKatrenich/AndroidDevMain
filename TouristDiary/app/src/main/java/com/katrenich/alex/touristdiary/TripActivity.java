e44946ad5805f4f57afdeca97126a8dbad69c1f9
AlexKatrenich <alex.katrenich@gmail.com>
2018-05-30 20:52:40 +0300

    Changed fragments

--- TouristDiary/app/src/main/java/com/katrenich/alex/touristdiary/TripActivity.java
+++ TouristDiary/app/src/main/java/com/katrenich/alex/touristdiary/TripActivity.java
@@ -1,13 +1,20 @@
 package com.katrenich.alex.touristdiary;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.ActionBar;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.katrenich.alex.touristdiary.fragments.TripMainFragment;
+import com.katrenich.alex.touristdiary.fragments.TripMapFragment;
+import com.katrenich.alex.touristdiary.fragments.TripNoteFragment;
+import com.katrenich.alex.touristdiary.fragments.TripPhotosFragment;
 
 public class TripActivity extends LogActivity {
-
     private ActionBar actionBar;
     private BottomNavigationView bottomNavigationView;
 
@@ -15,10 +22,10 @@ public class TripActivity extends LogActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_trip);
-        init();
+        init(savedInstanceState);
     }
 
-    private void init(){
+    private void init(Bundle savedInstanceState){
         // редагую верхнє меню, додаю кнопку "назад" та видаляю назву проекту
         actionBar = this.getSupportActionBar();
         Log.d(TAG, String.valueOf("init: actionBar = getActionBar()"));
@@ -37,6 +44,59 @@ public class TripActivity extends LogActivity {
 
         bottomNavigationView = findViewById(R.id.bnv_trip_activity);
 
+        if(savedInstanceState == null){
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .add(R.id.fl_trip_fragments, new TripMainFragment())
+                    .commit();
+        }
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                switch (id){
+                    case R.id.bnm_main :
+                        getSupportFragmentManager()
+                                .beginTransaction()
+                                .replace(R.id.fl_trip_fragments, new TripMainFragment())
+                                .addToBackStack(null)
+                                .commit();
+                        Toast.makeText(TripActivity.this, "TripMainFragment", Toast.LENGTH_SHORT).show();
+                        return true;
+                    case R.id.bnm_map :
+                        getSupportFragmentManager()
+                                .beginTransaction()
+                                .replace(R.id.fl_trip_fragments, new TripMapFragment())
+                                .addToBackStack(null)
+                                .commit();
+
+                        Toast.makeText(TripActivity.this, "TripMapFragment", Toast.LENGTH_SHORT).show();
+                        return true;
+                    case R.id.bnm_notes :
+                        getSupportFragmentManager()
+                                .beginTransaction()
+                                .replace(R.id.fl_trip_fragments, new TripNoteFragment())
+                                .addToBackStack(null)
+                                .commit();
+
+                        Toast.makeText(TripActivity.this, "TripNoteFragment", Toast.LENGTH_SHORT).show();
+                        return true;
+                    case R.id.bnm_photos :
+                        getSupportFragmentManager()
+                                .beginTransaction()
+                                .replace(R.id.fl_trip_fragments, new TripPhotosFragment())
+                                .addToBackStack(null)
+                                .commit();
+
+                        Toast.makeText(TripActivity.this, "TripPhotosFragment", Toast.LENGTH_SHORT).show();
+                        return true;
+                    default:
+                        return false;
+                }
+            }
+        });
+
     }
 
     @Override
@@ -45,8 +105,8 @@ public class TripActivity extends LogActivity {
             case android.R.id.home :
                 // потрібно дописати збереження змінених даних та повернення до головної активності
                 //TODO
-                onBackPressed();
-
+                startActivity(new Intent(this, MainActivity.class));
+                finish();
             default :
                 return false;
         }
--- TouristDiary/app/src/main/java/com/katrenich/alex/touristdiary/fragments/TripNoteFragment.java
+++ TouristDiary/app/src/main/java/com/katrenich/alex/touristdiary/fragments/TripNoteFragment.java
@@ -1,5 +1,37 @@
 package com.katrenich.alex.touristdiary.fragments;
 
-public class TripNoteFragment {
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.katrenich.alex.touristdiary.R;
+
+public class TripNoteFragment extends Fragment {
+    private static final String TAG = "Fragment";
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        Log.d(TAG, "onAttach");
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        Log.d(TAG, "onCreateView");
+        return inflater.inflate(R.layout.trip_notes_fragment, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        Log.d(TAG, "onViewCreated");
+        super.onViewCreated(view, savedInstanceState);
+    }
 
 }
